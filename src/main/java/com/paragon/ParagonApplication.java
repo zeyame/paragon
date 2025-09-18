7c41404a8dd9ea2ce43ac63e739aaa4e795a7646
Zeyad Mohamed <zeyame14@gmail.com>
2025-09-18 18:30:21 +0300

    Initial Commit

---
+++ src/main/java/com/paragon/ParagonApplication.java
@@ -0,0 +1,13 @@
+package com.paragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParagonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParagonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paragon/ParagonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paragon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParagonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
