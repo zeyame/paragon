d97841ded9ba2176d29f5da59f6b3000699c3073
zeyame <zeyame14@gmail.com>
2025-10-17 12:26:57 +0300

    Edit json properties for get endpoint

--- src/main/java/com/paragon/api/dtos/staffaccount/getall/GetAllStaffAccountsResponseDto.java
+++ src/main/java/com/paragon/api/dtos/staffaccount/getall/GetAllStaffAccountsResponseDto.java
@@ -1,5 +1,10 @@
 package com.paragon.api.dtos.staffaccount.getall;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
-public record GetAllStaffAccountsResponseDto(List<StaffAccountSummaryResponseDto> staffAccountSummaryResponseDtos) {}
+public record GetAllStaffAccountsResponseDto(
+        @JsonProperty("staff_accounts")
+        List<StaffAccountSummaryResponseDto> staffAccountSummaryResponseDtos
+) {}
--- src/main/java/com/paragon/api/dtos/staffaccount/getall/StaffAccountSummaryResponseDto.java
+++ src/main/java/com/paragon/api/dtos/staffaccount/getall/StaffAccountSummaryResponseDto.java
@@ -1,15 +1,21 @@
 package com.paragon.api.dtos.staffaccount.getall;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.Instant;
 import java.util.UUID;
 
 public record StaffAccountSummaryResponseDto(
+        @JsonProperty("id")
         UUID id,
+        @JsonProperty("username")
         String username,
+        @JsonProperty("status")
         String status,
+        @JsonProperty("order_access_duration")
         int orderAccessDuration,
+        @JsonProperty("modmail_transcript_access_duration")
         int modmailTranscriptAccessDuration,
+        @JsonProperty("created_at_utc")
         Instant createdAtUtc
-) {
-
-}
+) {}
