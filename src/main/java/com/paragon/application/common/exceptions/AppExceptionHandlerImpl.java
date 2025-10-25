1f67cd06c7ffcdc674dabbf0f07c72af11398509
zeyame <zeyame14@gmail.com>
2025-10-25 13:43:50 +0300

    Added logic for AppExceptionHandlerImpl and unit tested it.

--- src/main/java/com/paragon/application/common/exceptions/AppExceptionHandlerImpl.java
+++ src/main/java/com/paragon/application/common/exceptions/AppExceptionHandlerImpl.java
@@ -1,6 +1,10 @@
 package com.paragon.application.common.exceptions;
 
 import com.paragon.domain.exceptions.DomainException;
+import com.paragon.domain.exceptions.aggregate.StaffAccountException;
+import com.paragon.domain.exceptions.entity.AuditTrailEntryException;
+import com.paragon.domain.exceptions.entity.PermissionException;
+import com.paragon.domain.exceptions.valueobject.*;
 import com.paragon.infrastructure.persistence.exceptions.InfraException;
 import org.springframework.stereotype.Component;
 
@@ -8,11 +12,221 @@ import org.springframework.stereotype.Component;
 public class AppExceptionHandlerImpl implements AppExceptionHandler {
     @Override
     public AppException handleDomainException(DomainException domainException) {
-        return null;
+        return switch (domainException) {
+            case StaffAccountException staffAccountException -> handleStaffAccountException(staffAccountException);
+            case AuditTrailEntryException auditTrailEntryException -> handleAuditTrailEntryException(auditTrailEntryException);
+            case PermissionException permissionException -> handlePermissionException(permissionException);
+
+            // value object exceptions
+            case VersionException versionException -> handleVersionException(versionException);
+            case EventIdException eventIdException -> handleEventIdException(eventIdException);
+            case StaffAccountIdException staffAccountIdException -> handleStaffAccountIdException(staffAccountIdException);
+            case UsernameException usernameException -> handleUsernameException(usernameException);
+            case EmailException emailException -> handleEmailException(emailException);
+            case PasswordException passwordException -> handlePasswordException(passwordException);
+            case OrderAccessDurationException orderAccessDurationException -> handleOrderAccessDurationException(orderAccessDurationException);
+            case ModmailTranscriptAccessDurationException modmailTranscriptAccessDurationException -> handleModmailTranscriptAccessDurationException(modmailTranscriptAccessDurationException);
+            case FailedLoginAttemptsException failedLoginAttemptsException -> handleFailedLoginAttemptsException(failedLoginAttemptsException);
+            case PermissionIdException permissionIdException -> handlePermissionIdException(permissionIdException);
+            case PermissionCodeException permissionCodeException -> handlePermissionCodeException(permissionCodeException);
+            case AuditEntryIdException auditEntryIdException -> handleAuditEntryIdException(auditEntryIdException);
+            case AuditEntryTargetIdException auditEntryTargetIdException -> handleAuditEntryTargetIdException(auditEntryTargetIdException);
+
+            default -> new AppException(domainException, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
     }
 
     @Override
     public AppException handleInfraException(InfraException infraException) {
+        // TODO: Implement proper InfraException handling with InfraExceptionInfo
         return null;
+    }
+
+    private AppException handleStaffAccountException(StaffAccountException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 10001, 10002, 10003, 10004, 10005, 10006 -> // validation errors (required fields, at least one permission)
+                    new AppException(exception, AppExceptionStatusCode.CLIENT_ERROR);
+
+            case 10007 -> // account disabled
+                    new AppException(exception, AppExceptionStatusCode.INVALID_RESOURCE_STATE);
+
+            case 10008 -> // account locked
+                    new AppException(exception, AppExceptionStatusCode.INVALID_RESOURCE_STATE);
+
+            case 10009 -> // invalid credentials
+                    new AppException(exception, AppExceptionStatusCode.AUTHENTICATION_FAILED);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handleAuditTrailEntryException(AuditTrailEntryException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 201001, 201002, 201003 -> // validation errors (actorId, actionType, outcome required)
+                    new AppException(exception, AppExceptionStatusCode.SERVER_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handlePermissionException(PermissionException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 200001, 200002 -> // validation errors (code, category required) - internal entity creation
+                    new AppException(exception, AppExceptionStatusCode.SERVER_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handleVersionException(VersionException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 100001 -> // version must be at least 1 - internal error
+                    new AppException(exception, AppExceptionStatusCode.SERVER_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handleEventIdException(EventIdException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 101001 -> // event id cannot be null - internal error
+                    new AppException(exception, AppExceptionStatusCode.SERVER_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handleStaffAccountIdException(StaffAccountIdException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 102001, 102002 -> // missing value, invalid format - internal error
+                    new AppException(exception, AppExceptionStatusCode.SERVER_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handleUsernameException(UsernameException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 103001, 103002, 103003, 103004, 103005, 103006, 103007 -> // all username validation errors - user input
+                    new AppException(exception, AppExceptionStatusCode.CLIENT_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handleEmailException(EmailException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 104001, 104002, 104003 -> // missing value, length out of range, invalid format - user input
+                    new AppException(exception, AppExceptionStatusCode.CLIENT_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handlePasswordException(PasswordException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 105001, 105002, 105003, 105004, 105005, 105006, 105007, 105008 -> // all password validation errors - user input
+                    new AppException(exception, AppExceptionStatusCode.CLIENT_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handleOrderAccessDurationException(OrderAccessDurationException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 106001 -> // must be positive - user input (admin)
+                    new AppException(exception, AppExceptionStatusCode.CLIENT_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handleModmailTranscriptAccessDurationException(ModmailTranscriptAccessDurationException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 107001 -> // must be positive - user input (admin)
+                    new AppException(exception, AppExceptionStatusCode.CLIENT_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handleFailedLoginAttemptsException(FailedLoginAttemptsException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 108001 -> // invalid attempt number - internal error
+                    new AppException(exception, AppExceptionStatusCode.SERVER_ERROR);
+
+            case 108002 -> // max attempts reached - resource state issue
+                    new AppException(exception, AppExceptionStatusCode.INVALID_RESOURCE_STATE);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handlePermissionIdException(PermissionIdException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 109001, 109002 -> // missing value, invalid format - internal error
+                    new AppException(exception, AppExceptionStatusCode.SERVER_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handlePermissionCodeException(PermissionCodeException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 110001, 110002, 110003, 110004, 110005 -> // all permission code validation errors - user input (admin)
+                    new AppException(exception, AppExceptionStatusCode.CLIENT_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handleAuditEntryIdException(AuditEntryIdException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 111001, 111002 -> // missing value, invalid format - internal error
+                    new AppException(exception, AppExceptionStatusCode.SERVER_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
+    }
+
+    private AppException handleAuditEntryTargetIdException(AuditEntryTargetIdException exception) {
+        int domainErrorCode = exception.getDomainErrorCode();
+
+        return switch (domainErrorCode) {
+            case 112001 -> // missing value - internal error
+                    new AppException(exception, AppExceptionStatusCode.SERVER_ERROR);
+
+            default -> new AppException(exception, AppExceptionStatusCode.UNHANDLED_ERROR);
+        };
     }
 }
--- src/test/java/com/paragon/application/common/exceptions/AppExceptionHandlerTests.java
+++ src/test/java/com/paragon/application/common/exceptions/AppExceptionHandlerTests.java
@@ -1,4 +1,286 @@
 package com.paragon.application.common.exceptions;
 
+import com.paragon.domain.exceptions.DomainException;
+import com.paragon.domain.exceptions.aggregate.StaffAccountException;
+import com.paragon.domain.exceptions.aggregate.StaffAccountExceptionInfo;
+import com.paragon.domain.exceptions.entity.AuditTrailEntryException;
+import com.paragon.domain.exceptions.entity.AuditTrailEntryExceptionInfo;
+import com.paragon.domain.exceptions.entity.PermissionException;
+import com.paragon.domain.exceptions.entity.PermissionExceptionInfo;
+import com.paragon.domain.exceptions.valueobject.*;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 public class AppExceptionHandlerTests {
+    @Nested
+    class HandleDomainException {
+        private final AppExceptionHandlerImpl sut;
+
+        public HandleDomainException() {
+            this.sut = new AppExceptionHandlerImpl();
+        }
+
+        @ParameterizedTest
+        @MethodSource("provideDomainExceptions")
+        void shouldMapToCorrectAppException(DomainException domainException, AppExceptionStatusCode expectedStatusCode) {
+            // When
+            AppException result = sut.handleDomainException(domainException);
+
+            // Then
+            assertThat(result.getMessage()).isEqualTo(domainException.getMessage());
+            assertThat(result.getErrorCode()).isEqualTo(domainException.getDomainErrorCode());
+            assertThat(result.getStatusCode()).isEqualTo(expectedStatusCode);
+        }
+
+        private static Stream<Arguments> provideDomainExceptions() {
+            return Stream.of(
+                    // StaffAccountException - validation errors (CLIENT_ERROR)
+                    arguments(
+                            new StaffAccountException(StaffAccountExceptionInfo.usernameRequired()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new StaffAccountException(StaffAccountExceptionInfo.passwordRequired()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new StaffAccountException(StaffAccountExceptionInfo.orderAccessDurationRequired()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new StaffAccountException(StaffAccountExceptionInfo.modmailTranscriptAccessDurationRequired()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new StaffAccountException(StaffAccountExceptionInfo.createdByRequired()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new StaffAccountException(StaffAccountExceptionInfo.atLeastOnePermissionRequired()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+
+                    // StaffAccountException - account disabled (INVALID_RESOURCE_STATE)
+                    arguments(
+                            new StaffAccountException(StaffAccountExceptionInfo.disabled()),
+                            AppExceptionStatusCode.INVALID_RESOURCE_STATE
+                    ),
+
+                    // StaffAccountException - account locked (INVALID_RESOURCE_STATE)
+                    arguments(
+                            new StaffAccountException(StaffAccountExceptionInfo.locked()),
+                            AppExceptionStatusCode.INVALID_RESOURCE_STATE
+                    ),
+
+                    // StaffAccountException - invalid credentials (AUTHENTICATION_FAILED)
+                    arguments(
+                            new StaffAccountException(StaffAccountExceptionInfo.invalidCredentials()),
+                            AppExceptionStatusCode.AUTHENTICATION_FAILED
+                    ),
+
+                    // AuditTrailEntryException - internal errors (SERVER_ERROR)
+                    arguments(
+                            new AuditTrailEntryException(AuditTrailEntryExceptionInfo.actorIdRequired()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+                    arguments(
+                            new AuditTrailEntryException(AuditTrailEntryExceptionInfo.actionTypeRequired()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+                    arguments(
+                            new AuditTrailEntryException(AuditTrailEntryExceptionInfo.outcomeRequired()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+
+                    // PermissionException - internal errors (SERVER_ERROR)
+                    arguments(
+                            new PermissionException(PermissionExceptionInfo.codeRequired()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+                    arguments(
+                            new PermissionException(PermissionExceptionInfo.categoryRequired()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+
+                    // VersionException - internal error (SERVER_ERROR)
+                    arguments(
+                            new VersionException(VersionExceptionInfo.mustBeAtleastOne()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+
+                    // EventIdException - internal error (SERVER_ERROR)
+                    arguments(
+                            new EventIdException(EventIdExceptionInfo.mustNotBeNull()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+
+                    // StaffAccountIdException - internal error (SERVER_ERROR)
+                    arguments(
+                            new StaffAccountIdException(StaffAccountIdExceptionInfo.missingValue()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+                    arguments(
+                            new StaffAccountIdException(StaffAccountIdExceptionInfo.invalidFormat()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+
+                    // UsernameException - user input (CLIENT_ERROR)
+                    arguments(
+                            new UsernameException(UsernameExceptionInfo.missingValue()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new UsernameException(UsernameExceptionInfo.lengthOutOfRange()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new UsernameException(UsernameExceptionInfo.invalidCharacters()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new UsernameException(UsernameExceptionInfo.consecutiveUnderscores()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new UsernameException(UsernameExceptionInfo.mustStartWithALetter()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new UsernameException(UsernameExceptionInfo.mustNotEndWithUnderscore()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new UsernameException(UsernameExceptionInfo.reservedWord()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+
+                    // EmailException - user input (CLIENT_ERROR)
+                    arguments(
+                            new EmailException(EmailExceptionInfo.missingValue()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new EmailException(EmailExceptionInfo.lengthOutOfRange()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new EmailException(EmailExceptionInfo.invalidFormat()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+
+                    // PasswordException - user input (CLIENT_ERROR)
+                    arguments(
+                            new PasswordException(PasswordExceptionInfo.missingValue()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new PasswordException(PasswordExceptionInfo.tooShort(8)),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new PasswordException(PasswordExceptionInfo.tooLong(128)),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new PasswordException(PasswordExceptionInfo.missingUppercase()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new PasswordException(PasswordExceptionInfo.missingLowercase()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new PasswordException(PasswordExceptionInfo.missingDigit()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new PasswordException(PasswordExceptionInfo.missingSpecialCharacter()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new PasswordException(PasswordExceptionInfo.containsWhitespace()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+
+                    // OrderAccessDurationException - user input (CLIENT_ERROR)
+                    arguments(
+                            new OrderAccessDurationException(OrderAccessDurationExceptionInfo.mustBePositive()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+
+                    // ModmailTranscriptAccessDurationException - user input (CLIENT_ERROR)
+                    arguments(
+                            new ModmailTranscriptAccessDurationException(ModmailTranscriptAccessDurationExceptionInfo.mustBePositive()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+
+                    // FailedLoginAttemptsException - internal error (SERVER_ERROR)
+                    arguments(
+                            new FailedLoginAttemptsException(FailedLoginAttemptsExceptionInfo.invalidAttemptNumber()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+
+                    // FailedLoginAttemptsException - max attempts (INVALID_RESOURCE_STATE)
+                    arguments(
+                            new FailedLoginAttemptsException(FailedLoginAttemptsExceptionInfo.maxAttemptsReached()),
+                            AppExceptionStatusCode.INVALID_RESOURCE_STATE
+                    ),
+
+                    // PermissionIdException - internal error (SERVER_ERROR)
+                    arguments(
+                            new PermissionIdException(PermissionIdExceptionInfo.missingValue()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+                    arguments(
+                            new PermissionIdException(PermissionIdExceptionInfo.invalidFormat()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+
+                    // PermissionCodeException - user input (CLIENT_ERROR)
+                    arguments(
+                            new PermissionCodeException(PermissionCodeExceptionInfo.missingValue()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new PermissionCodeException(PermissionCodeExceptionInfo.lengthOutOfRange()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new PermissionCodeException(PermissionCodeExceptionInfo.invalidCharacters()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new PermissionCodeException(PermissionCodeExceptionInfo.mustNotStartOrEndWithUnderscore()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+                    arguments(
+                            new PermissionCodeException(PermissionCodeExceptionInfo.consecutiveUnderscores()),
+                            AppExceptionStatusCode.CLIENT_ERROR
+                    ),
+
+                    // AuditEntryIdException - internal error (SERVER_ERROR)
+                    arguments(
+                            new AuditEntryIdException(AuditEntryIdExceptionInfo.missingValue()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+                    arguments(
+                            new AuditEntryIdException(AuditEntryIdExceptionInfo.invalidFormat()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    ),
+
+                    // AuditEntryTargetIdException - internal error (SERVER_ERROR)
+                    arguments(
+                            new AuditEntryTargetIdException(AuditEntryTargetIdExceptionInfo.missingValue()),
+                            AppExceptionStatusCode.SERVER_ERROR
+                    )
+            );
+        }
+    }
 }
