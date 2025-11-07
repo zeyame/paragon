208abf383c4eab97e9ad6c785924fb23e66bbbb8
zeyame <zeyame14@gmail.com>
2025-11-07 16:49:11 +0200

    Clear any lingering transactions that may remain open on a worker thread after it finishes execution

--- src/main/java/com/paragon/infrastructure/config/AsyncConfig.java
+++ src/main/java/com/paragon/infrastructure/config/AsyncConfig.java
@@ -1,6 +1,8 @@
 package com.paragon.infrastructure.config;
 
+import com.paragon.infrastructure.persistence.jdbc.transaction.UnitOfWorkAwareDataSource;
 import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskDecorator;
@@ -12,11 +14,18 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
+import java.sql.SQLException;
 import java.util.Map;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+    private final UnitOfWorkAwareDataSource dataSource;
+
+    @Autowired
+    public AsyncConfig(UnitOfWorkAwareDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean(name = "taskExecutor")
     public TaskExecutor taskExecutor() {
@@ -25,12 +34,18 @@ public class AsyncConfig {
         executor.setMaxPoolSize(200);
         executor.setQueueCapacity(500);
         executor.setThreadNamePrefix("Async-");
-        executor.setTaskDecorator(new MdcAndSecurityContextTaskDecorator());
+        executor.setTaskDecorator(new MdcAndSecurityContextTaskDecorator(dataSource));
         executor.initialize();
         return executor;
     }
 
     static class MdcAndSecurityContextTaskDecorator implements TaskDecorator {
+        private final UnitOfWorkAwareDataSource dataSource;
+
+        public MdcAndSecurityContextTaskDecorator(UnitOfWorkAwareDataSource dataSource) {
+            this.dataSource = dataSource;
+        }
+
         @Override
         public Runnable decorate(Runnable runnable) {
             Map<String, String> contextMap = MDC.getCopyOfContextMap();
@@ -47,9 +62,18 @@ public class AsyncConfig {
 
                     runnable.run();
                 } finally {
+                    // clean up any lingering transactions on this worker thread
+                    if (dataSource.isTransactionActive()) {
+                        try {
+                            dataSource.rollbackTransaction();
+                        } catch (SQLException e) {
+                            // ignore
+                        }
+                    }
+
                     MDC.clear();
                     SecurityContextHolder.clearContext();
-                    org.springframework.web.context.request.RequestContextHolder.resetRequestAttributes();
+                    RequestContextHolder.resetRequestAttributes();
                 }
             };
         }
