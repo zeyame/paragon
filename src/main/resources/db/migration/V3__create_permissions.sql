65d03d96ba72532ca413605b7a59d09e739b88e3
zeyame <zeyame14@gmail.com>
2025-11-22 17:43:19 +0200

    stop tracking migration scripts

--- src/main/resources/db/migration/V3__create_permissions.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS permissions (
-    id              UUID PRIMARY KEY,
-    code            VARCHAR(64) NOT NULL UNIQUE,
-    category        VARCHAR(64) NOT NULL,
-    description     TEXT,
-    created_at_utc  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    updated_at_utc  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
-);
