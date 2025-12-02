62726edb7b4508686b116ba056f6466010a9abe9
zeyame <zeyame14@gmail.com>
2025-12-02 11:43:29 +0200

    Create StaffAccountPasswordChangedEvent

---
+++ src/main/java/com/paragon/domain/events/staffaccountevents/StaffAccountPasswordChangedEvent.java
@@ -0,0 +1,11 @@
+package com.paragon.domain.events.staffaccountevents;
+
+import com.paragon.domain.events.EventNames;
+import com.paragon.domain.models.aggregates.StaffAccount;
+
+public class StaffAccountPasswordChangedEvent extends StaffAccountEventBase {
+
+    public StaffAccountPasswordChangedEvent(StaffAccount staffAccount) {
+        super(staffAccount, EventNames.STAFF_ACCOUNT_PASSWORD_CHANGED_EVENT);
+    }
+}
