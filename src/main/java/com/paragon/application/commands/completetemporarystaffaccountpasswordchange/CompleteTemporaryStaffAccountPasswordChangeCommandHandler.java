92c74fc938f10f5e42de71ab861335460536f02e
zeyame <zeyame14@gmail.com>
2025-12-04 20:22:55 +0200

    Throw if staff account does not exist

--- src/main/java/com/paragon/application/commands/completetemporarystaffaccountpasswordchange/CompleteTemporaryStaffAccountPasswordChangeCommandHandler.java
+++ src/main/java/com/paragon/application/commands/completetemporarystaffaccountpasswordchange/CompleteTemporaryStaffAccountPasswordChangeCommandHandler.java
@@ -1,18 +1,27 @@
 package com.paragon.application.commands.completetemporarystaffaccountpasswordchange;
 
 import com.paragon.application.commands.CommandHandler;
+import com.paragon.application.common.exceptions.AppException;
+import com.paragon.application.common.exceptions.AppExceptionInfo;
 import com.paragon.application.common.interfaces.UnitOfWork;
+import com.paragon.domain.interfaces.StaffAccountWriteRepo;
+import com.paragon.domain.models.aggregates.StaffAccount;
+import com.paragon.domain.models.valueobjects.StaffAccountId;
 
 public class CompleteTemporaryStaffAccountPasswordChangeCommandHandler implements CommandHandler<CompleteTemporaryStaffAccountPasswordChangeCommand, CompleteTemporaryStaffAccountPasswordChangeResponse> {
+    private final StaffAccountWriteRepo staffAccountWriteRepoMock;
     private final UnitOfWork unitOfWork;
 
-    public CompleteTemporaryStaffAccountPasswordChangeCommandHandler(UnitOfWork unitOfWorkMock) {
+    public CompleteTemporaryStaffAccountPasswordChangeCommandHandler(StaffAccountWriteRepo staffAccountWriteRepoMock, UnitOfWork unitOfWorkMock) {
+        this.staffAccountWriteRepoMock = staffAccountWriteRepoMock;
         this.unitOfWork = unitOfWorkMock;
     }
 
     @Override
     public CompleteTemporaryStaffAccountPasswordChangeResponse handle(CompleteTemporaryStaffAccountPasswordChangeCommand command) {
         unitOfWork.begin();
+        StaffAccount staffAccount = staffAccountWriteRepoMock.getById(StaffAccountId.from(command.id()))
+                .orElseThrow(() -> new AppException(AppExceptionInfo.staffAccountNotFound(command.id())));
         return null;
     }
 }
--- src/test/java/com/paragon/application/commands/CompleteTemporaryStaffAccountPasswordChangeCommandHandlerTests.java
+++ src/test/java/com/paragon/application/commands/CompleteTemporaryStaffAccountPasswordChangeCommandHandlerTests.java
@@ -2,26 +2,56 @@ package com.paragon.application.commands;
 
 import com.paragon.application.commands.completetemporarystaffaccountpasswordchange.CompleteTemporaryStaffAccountPasswordChangeCommand;
 import com.paragon.application.commands.completetemporarystaffaccountpasswordchange.CompleteTemporaryStaffAccountPasswordChangeCommandHandler;
+import com.paragon.application.common.exceptions.AppException;
+import com.paragon.application.common.exceptions.AppExceptionInfo;
 import com.paragon.application.common.interfaces.UnitOfWork;
+import com.paragon.domain.interfaces.StaffAccountWriteRepo;
+import com.paragon.domain.models.valueobjects.StaffAccountId;
+import com.paragon.infrastructure.persistence.repos.StaffAccountWriteRepoImpl;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.*;
 
 public class CompleteTemporaryStaffAccountPasswordChangeCommandHandlerTests {
     private final CompleteTemporaryStaffAccountPasswordChangeCommandHandler sut;
+    private StaffAccountWriteRepo staffAccountWriteRepoMock;
     private UnitOfWork unitOfWorkMock;
 
     public CompleteTemporaryStaffAccountPasswordChangeCommandHandlerTests() {
+        staffAccountWriteRepoMock = mock(StaffAccountWriteRepo.class);
         unitOfWorkMock = mock(UnitOfWork.class);
-        sut = new CompleteTemporaryStaffAccountPasswordChangeCommandHandler(unitOfWorkMock);
+        sut = new CompleteTemporaryStaffAccountPasswordChangeCommandHandler(staffAccountWriteRepoMock, unitOfWorkMock);
     }
 
     @Test
     void shouldBeginTransaction() {
         // When
-        sut.handle(new CompleteTemporaryStaffAccountPasswordChangeCommand("new-password"));
+        sut.handle(new CompleteTemporaryStaffAccountPasswordChangeCommand(UUID.randomUUID().toString(), "new-password"));
 
         // Then
         verify(unitOfWorkMock, times(1)).begin();
+    }
+
+    @Test
+    void shouldThrowIfStaffAccountDoesNotExist() {
+        // Given
+        StaffAccountId staffAccountId = StaffAccountId.generate();
+        CompleteTemporaryStaffAccountPasswordChangeCommand command = new CompleteTemporaryStaffAccountPasswordChangeCommand(
+                staffAccountId.getValue().toString(),
+                "new-password"
+        );
+        when(staffAccountWriteRepoMock.getById(staffAccountId))
+                .thenReturn(Optional.empty());
+        AppException expectedException = new AppException(AppExceptionInfo.staffAccountNotFound(staffAccountId.getValue().toString()));
+
+        // When & Then
+        assertThatExceptionOfType(AppException.class)
+                .isThrownBy(() -> sut.handle(command))
+                .extracting("message", "errorCode", "statusCode")
+                .containsExactly(expectedException.getMessage(), expectedException.getErrorCode(), expectedException.getStatusCode());
     }
 }
