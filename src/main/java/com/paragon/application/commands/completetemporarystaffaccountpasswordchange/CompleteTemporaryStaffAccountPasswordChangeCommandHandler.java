9c783848425a02e37abff541baf0b24483124008
zeyame <zeyame14@gmail.com>
2025-12-04 21:06:57 +0200

    Catch DomainException

--- src/main/java/com/paragon/application/commands/completetemporarystaffaccountpasswordchange/CompleteTemporaryStaffAccountPasswordChangeCommandHandler.java
+++ src/main/java/com/paragon/application/commands/completetemporarystaffaccountpasswordchange/CompleteTemporaryStaffAccountPasswordChangeCommandHandler.java
@@ -3,36 +3,45 @@ package com.paragon.application.commands.completetemporarystaffaccountpasswordch
 import com.paragon.application.commands.CommandHandler;
 import com.paragon.application.common.exceptions.AppException;
 import com.paragon.application.common.exceptions.AppExceptionInfo;
+import com.paragon.application.common.interfaces.AppExceptionHandler;
 import com.paragon.application.common.interfaces.PasswordHasher;
 import com.paragon.application.common.interfaces.UnitOfWork;
+import com.paragon.domain.exceptions.DomainException;
 import com.paragon.domain.interfaces.StaffAccountWriteRepo;
 import com.paragon.domain.models.aggregates.StaffAccount;
 import com.paragon.domain.models.valueobjects.Password;
+import com.paragon.domain.models.valueobjects.PlaintextPassword;
 import com.paragon.domain.models.valueobjects.StaffAccountId;
 
 public class CompleteTemporaryStaffAccountPasswordChangeCommandHandler implements CommandHandler<CompleteTemporaryStaffAccountPasswordChangeCommand, CompleteTemporaryStaffAccountPasswordChangeResponse> {
     private final StaffAccountWriteRepo staffAccountWriteRepo;
     private final UnitOfWork unitOfWork;
     private final PasswordHasher passwordHasher;
+    private final AppExceptionHandler appExceptionHandler;
 
-    public CompleteTemporaryStaffAccountPasswordChangeCommandHandler(StaffAccountWriteRepo staffAccountWriteRepo, UnitOfWork unitOfWork, PasswordHasher passwordHasher) {
+    public CompleteTemporaryStaffAccountPasswordChangeCommandHandler(StaffAccountWriteRepo staffAccountWriteRepo, UnitOfWork unitOfWork, PasswordHasher passwordHasher, AppExceptionHandler appExceptionHandler) {
         this.staffAccountWriteRepo = staffAccountWriteRepo;
         this.unitOfWork = unitOfWork;
         this.passwordHasher = passwordHasher;
+        this.appExceptionHandler = appExceptionHandler;
     }
 
     @Override
     public CompleteTemporaryStaffAccountPasswordChangeResponse handle(CompleteTemporaryStaffAccountPasswordChangeCommand command) {
-        unitOfWork.begin();
-        StaffAccount staffAccount = staffAccountWriteRepo.getById(StaffAccountId.from(command.id()))
-                .orElseThrow(() -> new AppException(AppExceptionInfo.staffAccountNotFound(command.id())));
-        if (isTheSamePassword(command.newPassword(), staffAccount.getPassword())) {
-            throw new AppException(AppExceptionInfo.newPasswordMatchesCurrentPassword());
+        try {
+            unitOfWork.begin();
+            StaffAccount staffAccount = staffAccountWriteRepo.getById(StaffAccountId.from(command.id()))
+                    .orElseThrow(() -> new AppException(AppExceptionInfo.staffAccountNotFound(command.id())));
+            if (isTheSamePassword(PlaintextPassword.of(command.newPassword()), staffAccount.getPassword())) {
+                throw new AppException(AppExceptionInfo.newPasswordMatchesCurrentPassword());
+            }
+        } catch (DomainException ex) {
+            throw appExceptionHandler.handleDomainException(ex);
         }
         return null;
     }
 
-    private boolean isTheSamePassword(String newPassword, Password currentPassword) {
-        return passwordHasher.verify(newPassword, currentPassword);
+    private boolean isTheSamePassword(PlaintextPassword newPassword, Password currentPassword) {
+        return passwordHasher.verify(newPassword.getValue(), currentPassword);
     }
 }
--- src/test/java/com/paragon/application/commands/CompleteTemporaryStaffAccountPasswordChangeCommandHandlerTests.java
+++ src/test/java/com/paragon/application/commands/CompleteTemporaryStaffAccountPasswordChangeCommandHandlerTests.java
@@ -1,45 +1,57 @@
 package com.paragon.application.commands;
 
-import ch.qos.logback.classic.spi.LoggingEvent;
 import com.paragon.application.commands.completetemporarystaffaccountpasswordchange.CompleteTemporaryStaffAccountPasswordChangeCommand;
 import com.paragon.application.commands.completetemporarystaffaccountpasswordchange.CompleteTemporaryStaffAccountPasswordChangeCommandHandler;
 import com.paragon.application.common.exceptions.AppException;
 import com.paragon.application.common.exceptions.AppExceptionInfo;
+import com.paragon.application.common.interfaces.AppExceptionHandler;
 import com.paragon.application.common.interfaces.PasswordHasher;
 import com.paragon.application.common.interfaces.UnitOfWork;
-import com.paragon.domain.exceptions.aggregate.StaffAccountException;
-import com.paragon.domain.exceptions.aggregate.StaffAccountExceptionInfo;
+import com.paragon.domain.exceptions.DomainException;
 import com.paragon.domain.interfaces.StaffAccountWriteRepo;
 import com.paragon.domain.models.aggregates.StaffAccount;
 import com.paragon.domain.models.valueobjects.Password;
+import com.paragon.domain.models.valueobjects.PlaintextPassword;
 import com.paragon.domain.models.valueobjects.StaffAccountId;
 import com.paragon.helpers.fixtures.StaffAccountFixture;
-import com.paragon.infrastructure.persistence.repos.StaffAccountWriteRepoImpl;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.Mockito.*;
 
 public class CompleteTemporaryStaffAccountPasswordChangeCommandHandlerTests {
     private final CompleteTemporaryStaffAccountPasswordChangeCommandHandler sut;
+    private final CompleteTemporaryStaffAccountPasswordChangeCommand command;
     private StaffAccountWriteRepo staffAccountWriteRepoMock;
     private UnitOfWork unitOfWorkMock;
     private PasswordHasher passwordHasherMock;
+    private AppExceptionHandler appExceptionHandlerMock;
 
     public CompleteTemporaryStaffAccountPasswordChangeCommandHandlerTests() {
         staffAccountWriteRepoMock = mock(StaffAccountWriteRepo.class);
         unitOfWorkMock = mock(UnitOfWork.class);
         passwordHasherMock = mock(PasswordHasher.class);
-        sut = new CompleteTemporaryStaffAccountPasswordChangeCommandHandler(staffAccountWriteRepoMock, unitOfWorkMock, passwordHasherMock);
+        appExceptionHandlerMock = mock(AppExceptionHandler.class);
+        sut = new CompleteTemporaryStaffAccountPasswordChangeCommandHandler(staffAccountWriteRepoMock, unitOfWorkMock, passwordHasherMock, appExceptionHandlerMock);
+
+        // set up happy case
+        StaffAccount staffAccount = StaffAccountFixture.validStaffAccount();
+        command = new CompleteTemporaryStaffAccountPasswordChangeCommand(
+                staffAccount.getId().getValue().toString(),
+                PlaintextPassword.generate().getValue()
+        );
+        when(staffAccountWriteRepoMock.getById(any(StaffAccountId.class)))
+                .thenReturn(Optional.of(staffAccount));
+        when(passwordHasherMock.verify(command.newPassword(), staffAccount.getPassword()))
+                .thenReturn(false);
     }
 
     @Test
     void shouldBeginTransaction() {
         // When
-        sut.handle(new CompleteTemporaryStaffAccountPasswordChangeCommand(UUID.randomUUID().toString(), "new-password"));
+        sut.handle(command);
 
         // Then
         verify(unitOfWorkMock, times(1)).begin();
@@ -48,11 +60,7 @@ public class CompleteTemporaryStaffAccountPasswordChangeCommandHandlerTests {
     @Test
     void shouldThrowIfStaffAccountDoesNotExist() {
         // Given
-        StaffAccountId staffAccountId = StaffAccountId.generate();
-        CompleteTemporaryStaffAccountPasswordChangeCommand command = new CompleteTemporaryStaffAccountPasswordChangeCommand(
-                staffAccountId.getValue().toString(),
-                "new-password"
-        );
+        StaffAccountId staffAccountId = StaffAccountId.from(command.id());
         when(staffAccountWriteRepoMock.getById(staffAccountId))
                 .thenReturn(Optional.empty());
         AppException expectedException = new AppException(AppExceptionInfo.staffAccountNotFound(staffAccountId.getValue().toString()));
@@ -67,22 +75,30 @@ public class CompleteTemporaryStaffAccountPasswordChangeCommandHandlerTests {
     @Test
     void shouldThrowIfEnteredPasswordIsTheSameAsCurrentOne() {
         // Given
-        StaffAccount staffAccount = StaffAccountFixture.validStaffAccount();
-        CompleteTemporaryStaffAccountPasswordChangeCommand command = new CompleteTemporaryStaffAccountPasswordChangeCommand(
-                staffAccount.getId().getValue().toString(),
-                "new-password"
-        );
-        when(staffAccountWriteRepoMock.getById(any(StaffAccountId.class)))
-                .thenReturn(Optional.of(staffAccount));
-        when(passwordHasherMock.verify(command.newPassword(), staffAccount.getPassword()))
+        when(passwordHasherMock.verify(anyString(), any(Password.class)))
                 .thenReturn(true);
         AppException expectedException = new AppException(AppExceptionInfo.newPasswordMatchesCurrentPassword());
-
 
         // When & Then
         assertThatExceptionOfType(AppException.class)
                 .isThrownBy(() -> sut.handle(command))
                 .extracting("message", "errorCode", "statusCode")
                 .containsExactly(expectedException.getMessage(), expectedException.getErrorCode(), expectedException.getStatusCode());
+    }
+
+    @Test
+    void shouldCatchDomainException_andTranslateToAppException() {
+        // Given
+        StaffAccountId staffAccountId = StaffAccountId.generate();
+        CompleteTemporaryStaffAccountPasswordChangeCommand invalidCommand = new CompleteTemporaryStaffAccountPasswordChangeCommand(
+                staffAccountId.getValue().toString(),
+                "new-password"      // invalid password
+        );
+        when(appExceptionHandlerMock.handleDomainException(any(DomainException.class)))
+                .thenReturn(mock(AppException.class));
+
+        // When & Then
+        assertThatExceptionOfType(AppException.class)
+                .isThrownBy(() -> sut.handle(invalidCommand));
     }
 }
