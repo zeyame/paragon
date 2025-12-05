a37fa35dd410a5579ba33af330293eb05094b509
zeyame <zeyame14@gmail.com>
2025-12-05 10:49:18 +0200

    Rename

--- src/main/java/com/paragon/domain/events/staffaccountevents/StaffAccountPasswordChangedEvent.java
+++ src/main/java/com/paragon/domain/events/staffaccountevents/StaffAccountPasswordChangedEvent.java
@@ -6,6 +6,6 @@ import com.paragon.domain.models.aggregates.StaffAccount;
 public class StaffAccountPasswordChangedEvent extends StaffAccountEventBase {
 
     public StaffAccountPasswordChangedEvent(StaffAccount staffAccount) {
-        super(staffAccount, EventNames.STAFF_ACCOUNT_PASSWORD_CHANGED_EVENT);
+        super(staffAccount, EventNames.STAFF_ACCOUNT_PASSWORD_CHANGED);
     }
 }
