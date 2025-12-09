a0cd5d88bee22069c0aa828ebde8cdbf5fd699ad
zeyame <zeyame14@gmail.com>
2025-12-09 20:44:32 +0200

    Remove StaffAccountAuthorizationService.java

--- src/main/java/com/paragon/application/services/StaffAccountAuthorizationService.java
+++
@@ -1,8 +0,0 @@
-package com.paragon.application.services;
-
-import com.paragon.domain.models.valueobjects.PermissionCode;
-import com.paragon.domain.models.valueobjects.StaffAccountId;
-
-public interface StaffAccountAuthorizationService {
-    void authorizeAction(StaffAccountId staffAccountId, PermissionCode requiredPermission);
-}
