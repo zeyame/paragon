d3eefad04737be03b0b8ee7770f29f6aad12dbc0
zeyame <zeyame14@gmail.com>
2025-12-20 19:43:40 +0200

    Delete CheckPendingStaffAccountRequestService

--- src/main/java/com/paragon/application/common/interfaces/CheckPendingStaffAccountRequestService.java
+++
@@ -1,8 +0,0 @@
-package com.paragon.application.common.interfaces;
-
-import com.paragon.domain.enums.StaffAccountRequestType;
-import com.paragon.domain.models.valueobjects.StaffAccountId;
-
-public interface CheckPendingStaffAccountRequestService {
-    void ensureNoPendingRequest(StaffAccountId staffAccountId, StaffAccountRequestType requestType);
-}
